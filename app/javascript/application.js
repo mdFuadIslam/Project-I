cbb1f85b5a31c62a27e5c5af69b66786399cf385
Md Fuad Islam <fuadislamofficial@gmail.com>
2024-05-22 13:06:36 +0600

    Start

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import 'flowbite';
+import "flowbite/dist/flowbite.turbo.js";
+
+//= require node_modules/flowbite/dist/flowbite.min.js
