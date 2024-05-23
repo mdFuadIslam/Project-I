392eee0e9fcc573ca94ec3ce4b19b74140e86385
Md Fuad Islam <fuadislamofficial@gmail.com>
2024-05-23 16:45:26 +0600

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module ProjectI
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.available_locale = [:en, :bn]
+    config.i18n.available_locales = [:en, :bn]
 
     config.i18n.default_locale = :en
 
