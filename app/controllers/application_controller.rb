0434c38797a3106ba3cd06cc046138b31209a171
Md Fuad Islam <fuadislamofficial@gmail.com>
2024-05-26 19:46:09 +0600

    Css added to sign in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+       def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :status, :theme, :language, :nationality, :city)}
+
+            devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :status, :theme, :language, :nationality, :city)}
+       end
 
 end
