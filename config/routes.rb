eb5e3aa50180ddee74dbb28158e1fc360fbeba30
Md Fuad Islam <fuadislamofficial@gmail.com>
2024-05-30 00:12:38 +0600

    working search bar added

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   post 'home/comment', to: 'home#comment'
 
   post 'home/search_results', to: 'home#search_results'
+  post 'home/search', to: 'home#search_results'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
