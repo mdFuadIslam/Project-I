a71001558ebdd612fb7b3e610047093b79d63674
Md Fuad Islam <fuadislamofficial@gmail.com>
2024-05-30 13:53:33 +0600

    Polished V1

--- config/routes.rb
+++ config/routes.rb
@@ -32,6 +32,7 @@ Rails.application.routes.draw do
 
   post 'home/search_results', to: 'home#search_results'
   post 'home/search', to: 'home#search_results'
+  get 'home/search', to: 'home#search_results'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
